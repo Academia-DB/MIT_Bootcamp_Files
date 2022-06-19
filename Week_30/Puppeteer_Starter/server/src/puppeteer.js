f045cf2d5b3ccf5a8f0086689dca5d3bcd69be35
Derek <derekborowinski@gmail.com>
2022-06-18 22:08:56 -0400

    Update puppeteer.js

--- Week_30/Puppeteer_Starter/server/src/puppeteer.js
+++ Week_30/Puppeteer_Starter/server/src/puppeteer.js
@@ -2,16 +2,19 @@ const puppeteer = require('puppeteer');
 
 const testCreateUserButton = async () => {
   // Step 1 - Launch Puppeteer and assign response to the browser variable
-  let browser;
+  let browser = await puppeteer.launch();
 
   // Step 2 - Using the browser variable, create a new page and assign the response to the page variable
-  let page;
+  let page = await browser.newPage();
 
   // Step 3 - Using the page variable goto the correct URL that the React app is running on
+  await page.goto("http://localhost:3000/addUser.html");
 
   // Step 4 - Using the page variable, use the click method by passing the ".create-user" class name
+  await page.click('#addUser');
 
   // Step 5 - Using the page variable, use the screenshot method to generate a screenshot of the React app with the newly created user
+  await page.screenshot({path: 'screen.png'});
 };
 
 module.exports = testCreateUserButton;
